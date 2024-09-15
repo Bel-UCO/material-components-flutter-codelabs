f34dd6f52e650e4fd4eafb8c6cbf366f71ece304
bpoetridhanti <bpoetridhanti@student.ciputra.ac.id>
2024-09-15 19:59:12 +0000

    103

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -17,6 +17,8 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 import 'login.dart';
 
+import 'colors.dart';
+
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
   const ShrineApp({Key? key}) : super(key: key);
@@ -34,11 +36,27 @@ class ShrineApp extends StatelessWidget {
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      // TODO: Customize the theme (103)
-      theme: ThemeData.light(useMaterial3: true),
+  // TODO: Customize the theme (103)
+      theme: _kShrineTheme, // New code
     );
   }
 }
 
 // TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light(useMaterial3: true);
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
+
 // TODO: Build a Shrine Text Theme (103)
